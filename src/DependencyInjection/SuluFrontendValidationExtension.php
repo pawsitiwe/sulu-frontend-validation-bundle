5e87c03326242d2f462ec40a9fb75460ab8b2630
pawsitiwe <147261929+pawsitiwe@users.noreply.github.com>
2024-07-28 17:13:46 +0200

    Create SuluFrontendValidationExtension.php

---
+++ src/DependencyInjection/SuluFrontendValidationExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pawsitiwe\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class SuluFrontendValidationExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
